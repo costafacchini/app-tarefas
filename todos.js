536e48919640e507a905c0824219081418f9a465
Alan Costa Facchini <costafacchini@gmail.com>
2019-09-29 13:02:53 -0300

    Adicionar tarefa

--- todos.js
+++ todos.js
@@ -7,3 +7,26 @@ var todos = [
   'Estudar Javascript',
   'Estudar ReactJS'
 ];
+
+function renderTodos() {
+  listElement.innerHTML = '';
+  for (todo of todos) {
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
+
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodos();
+
+function addTodo() {
+  var todoText = inputElement.value;
+
+  todos.push(todoText);
+  inputElement.value = '';
+  renderTodos();
+}
+
+buttonElement.onclick = addTodo;
