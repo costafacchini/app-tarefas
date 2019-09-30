0497ff48dd1e43527672b983549837a86138639e
Alan Costa Facchini <costafacchini@gmail.com>
2019-09-29 22:50:23 -0300

    Gravar em banco local

--- todos.js
+++ todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-  'Fazer café',
-  'Estudar Javascript',
-  'Estudar ReactJS'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
   listElement.innerHTML = '';
@@ -39,6 +35,7 @@ function addTodo() {
   todos.push(todoText);
   inputElement.value = '';
   renderTodos();
+  saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -46,4 +43,9 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
   todos.splice(pos, 1);
   renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('list_todos', JSON.stringify(todos))
 }
